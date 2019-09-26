f60f9307298d190326a9f8c2f0eedf8d4ff18649
Nick <nschantz21@gmail.com>
2019-09-26 17:45:04 +0000

    completed first exercise

---
+++ A1/E1/C2A1E1_Macros.h
@@ -0,0 +1,8 @@
+#ifndef C2A1E1_MACROS_H
+#define C2A1E1_MACROS_H
+
+#define Product(ARTH1, ARTH2) ((ARTH1) * (ARTH2))
+#define Negate(NUM) (-(NUM))
+#define Elements(COLL) (sizeof(COLL)/sizeof(COLL[0]))
+
+#endif
