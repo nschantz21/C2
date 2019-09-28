edb6a7d630fbc91b2d2009ceed61c6e73ae9f979
Nick <nschantz21@gmail.com>
2019-09-28 22:39:53 +0000

    added comments to C2A1E2

--- A1/E2/C2A1E2_main.c
+++ A1/E2/C2A1E2_main.c
@@ -1,3 +1,19 @@
+/*
+ * Nicholas Schantz U08416544
+ * nschantz21@gmail.com
+ * C/C++ Programming II
+ * 142605 Ray Mitchell
+ * 2019-09-28
+ * C2A1E2_main.c
+ * Linux Ubuntu 16.04.4 LTS
+ * g++ 5.4.0
+ * 
+ * Display a count of the number of command line arguments that were present when
+ * the program was started followed by those arguments in their original order
+ * starting with argv[0] . The count and each argument must be displayed alone
+ * on separate lines.
+ */
+
 #include <stdio.h>
 
 int main(int argc, const char* argv[])
