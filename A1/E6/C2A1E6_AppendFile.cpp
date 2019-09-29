aeefbbd65111f34f23f0c3cf61708ccf76eb7a19
Nick <nschantz21@gmail.com>
2019-09-29 01:53:31 +0000

    added comments. added extra file closure handling

--- A1/E6/C2A1E6_AppendFile.cpp
+++ A1/E6/C2A1E6_AppendFile.cpp
@@ -4,24 +4,20 @@
  * C/C++ Programming II
  * 142605 Ray Mitchell
  * 2019-09-28
- * C2A1E5_ResizeAlloc.c
+ * C2A1E6_AppendFile.cpp
  * Linux Ubuntu 16.04.4 LTS
  * g++ 5.4.0
  * 
- * Program defines a function named AppendFile that returns type int and has two
- * parameters of type “pointer to constant char”, where the first is named inFile
- * and the second is named outFile. Each represents a string that specifies the
- * name of a file, such as file.c, test.txt, MyFile, etc. AppendFile appends 
- * the contents of the file specified by inFile onto the file specified by
- * outFile, creating the output file if it doesn’t already exist.
- *
+ * Program to concatenate one file to another. Create the output file if is does
+ * not yet exist. Works with both text and binary files.
  */
 
 #include <fstream>
 #include <iostream>
 using namespace std;
 
-int AppendFile(const char *inFile, const char *outFile) {    
+int AppendFile(const char *inFile, const char *outFile)
+{    
     /* open a file as binary */
     ifstream ifile(inFile, ios::in | ios::binary);
     
@@ -32,13 +28,14 @@ int AppendFile(const char *inFile, const char *outFile) {
         /* open the out file and check it. use ios::app to append to file */
         ofstream ofile(outFile, ios::out | ios::app | ios::binary);
         if (!ofile.is_open()) {
+            ifile.close();
             cerr << -1;
         } else {
             ofile << ifile.rdbuf();
-            /* close files to end the funciton */
-            ofile.close();    
+            /* close files to end the function */
+            ofile.close();
+            ifile.close();            
         }
     }
-    ifile.close();
     return 0;
 }
