9ade461696ed7fb2f7cb333939772df57cf49753
Nick <nschantz21@gmail.com>
2019-09-29 01:14:43 +0000

    added char type cast

--- A1/E4/C2A1E4_StrToUpper.c
+++ A1/E4/C2A1E4_StrToUpper.c
@@ -17,13 +17,14 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-size_t StrToUpper(char *s1, const char *s2) {
+size_t StrToUpper(char *s1, const char *s2)
+{
     /* create reference to beginnning of source string */
     const char * const START = s2;
     
     /* copy and convert source to target */
     while (*s2) {
-        *s1++ = toupper(*s2++);
+        *s1++ = (char) toupper(*s2++);
     }
     
     /* add null pointer to end of the target string */
