a023150ab8377fa87dc7cad8e5f4fa2a4f3395cb
Nick <nschantz21@gmail.com>
2019-09-29 00:34:11 +0000

    commented E3

--- A1/E3/C2A1E3_FindFirstInt.c
+++ A1/E3/C2A1E3_FindFirstInt.c
@@ -1,15 +1,28 @@
+/*
+ * Nicholas Schantz U08416544
+ * nschantz21@gmail.com
+ * C/C++ Programming II
+ * 142605 Ray Mitchell
+ * 2019-09-28
+ * C2A1E3_FindFirstInt.c
+ * Linux Ubuntu 16.04.4 LTS
+ * g++ 5.4.0
+ * 
+ * FindFirstInt must find the first occurrence of the value represented by value
+ * in the array represented ptr , which has count elements. If the value is found
+ * a pointer to that element is returned. Otherwise, a null pointer is returned.
+ */
+
 #include <stddef.h>
 #include <stdio.h>
 
 int *FindFirstInt(const int *ptr, size_t count, int value) {
-    
     size_t counter = 0;
-    
     while (counter < count) {
-        
+        /* required to cast ptr to int pointer from const int pointer */
         if (ptr[counter] == value) return (int *)&ptr[counter];
-        
         counter++;
     }
+    /* return null pointer if value not found in array */
     return NULL;
 }
--- A1/E4/C2A1E4_StrToUpper.c
+++ A1/E4/C2A1E4_StrToUpper.c
@@ -1,3 +1,19 @@
+/*
+ * Nicholas Schantz U08416544
+ * nschantz21@gmail.com
+ * C/C++ Programming II
+ * 142605 Ray Mitchell
+ * 2019-09-28
+ * C2A1E4_StrToUpper.c
+ * Linux Ubuntu 16.04.4 LTS
+ * g++ 5.4.0
+ * 
+ * StrToUpper copies the string represented by its second parameter into the
+ * memory represented by its first parameter, with any lowercase characters
+ * converted to uppercase. The length of the string, not including its null
+ * terminator character, is returned.
+ */
+
 #include <stdlib.h>
 #include <ctype.h>
 
