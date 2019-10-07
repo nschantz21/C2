0cdc88fec146afa8a577bd0429645ff8a5aad7b7
Nick <nschantz21@gmail.com>
2019-10-07 00:45:31 +0000

    syntax revisions

--- A2/E4/C2A2E4_OpenFile.cpp
+++ A2/E4/C2A2E4_OpenFile.cpp
@@ -1,4 +1,12 @@
 /*
+ * Nicholas Schantz U08416544
+ * nschantz21@gmail.com
+ * C/C++ Programming II
+ * 142605 Ray Mitchell
+ * 2019-10-04
+ * C2A2E4_OpenFile.cpp
+ * Linux Ubuntu 16.04.4 LTS
+ * g++ 5.4.0
  * 
  * OpenFile opens the file named in fileName in the read-only text mode using the inFile
  * object. If the open fails an error message is output to cerr and the program
@@ -7,13 +15,18 @@
  */
 #include <fstream>
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
 
 void OpenFile(const char *filename, ifstream &inFile)
 {
-    inFile.open(filename, ios::in);
+    /* don't need ios::in */
+    inFile.open(filename);
+    /* check if file successfully opened */
     if (!inFile.is_open()) {
         cerr << filename << " failed to open!\n";
+        exit(EXIT_FAILURE);
     } else {
         return;
     }
--- A2/E4/C2A2E4_Reverse.cpp
+++ A2/E4/C2A2E4_Reverse.cpp
@@ -1,30 +1,35 @@
 /*
+ * Nicholas Schantz U08416544
+ * nschantz21@gmail.com
+ * C/C++ Programming II
+ * 142605 Ray Mitchell
+ * 2019-10-04
+ * C2A2E4_Reverse.cpp
+ * Linux Ubuntu 16.04.4 LTS
+ * g++ 5.4.0
  * 
  * Recursively reads one character at a time from the text file in inFile until
  * a separator is encountered. Those non-separator characters are then displayed
  * in reverse order, with the last and next to next to last characters displayed
  * being capitalized. Finally, the separator is returned to the calling function.
  * Separators are not reversed and are not printed by Reverse, but are instead
- * merely returned. The code in the instructor-supplied driver file is
- * responsible for printing the separators.
+ * merely returned.
  */
 #include <fstream>
 #include <iostream>
 
 using namespace std;
 
-inline bool is_separator(char sep)
+const int CAPLAST = 1;
+const int CAPLASTLAST = 3;
+
+inline bool is_separator(int sep)
 {
     /* check is character is a separator */
-    if (
-        isspace(sep) ||
-        sep == '.' ||
-        sep == '?' ||
-        sep == '!' ||
-        sep == ',' ||
-        sep == ':' ||
-        sep == ';' ||
-        sep == EOF) {
+    if (isspace(sep) || (sep == '.') ||
+        (sep == '?') || (sep == '!') || 
+        (sep == ',') || (sep == ':') ||
+        (sep == ';') || (sep == EOF)) {
         return true;
     } else {
         return false;
@@ -33,19 +38,20 @@ inline bool is_separator(char sep)
 
 int Reverse(ifstream &inFile, const int level)
 {
-    /* get next char */
-    char this_char, this_separator;
-    inFile.get(this_char);
+    /* get next char from file stream */
+    int this_char, this_separator;
+    this_char = inFile.get();
+    /* if the char is a separator return to caller */
     if (is_separator(this_char)) {
         return this_char;
     } else {
-         this_separator = Reverse(inFile, level+1);
+        /* recursively call the function with increased level */
+        this_separator = Reverse(inFile, level + 1);
     }
-    
-    if (level == 1) {
+    /* capitalize if last or third to last in word */
+    if ((level == CAPLAST) || (level == CAPLASTLAST)) {
         this_char = toupper(this_char);
     }
-    cout << this_char;
-    
+    cout << (char)this_char;
     return this_separator;
 }
