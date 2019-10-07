7f5f005524cc26869089191bd74ae01a785242a1
Nick <nschantz21@gmail.com>
2019-10-07 03:57:28 +0000

    changing shifting operators and remove extern

--- A2/E2/C2A2E2_CountIntBitsF.cpp
+++ A2/E2/C2A2E2_CountIntBitsF.cpp
@@ -1,8 +1,25 @@
 /*
+ * Nicholas Schantz U08416544
+ * nschantz21@gmail.com
+ * C/C++ Programming II
+ * 142605 Ray Mitchell
+ * 2019-10-08
+ * C2A2E2_CountIntBitsF.cpp
+ * Linux Ubuntu 16.04.4 LTS
+ * g++ 5.4.0
  * 
+ * Determine the number of bits used to represent a type int value on any
+ * machine on which the program is run.
  */
 
 int CountIntBitsF()
 {
-    return (int)sizeof(int);
+    unsigned int get_size = 1;
+    int bit_count;
+    for (bit_count = 0; get_size != 0; bit_count++)
+    {
+        /* shift and update get_size left 1 */
+        get_size <<= 1;
+    }
+    return bit_count;
 }
--- A2/E2/C2A2E2_Rotate.cpp
+++ A2/E2/C2A2E2_Rotate.cpp
@@ -1,17 +1,27 @@
 /*
+ * Nicholas Schantz U08416544
+ * nschantz21@gmail.com
+ * C/C++ Programming II
+ * 142605 Ray Mitchell
+ * 2019-10-08
+ * C2A2E2_Rotate.cpp
+ * Linux Ubuntu 16.04.4 LTS
+ * g++ 5.4.0
  * 
+ * Program produces the value of parameterobject as if it had been rotated by the
+ * number of positions and in the direction specified by count.
  */
 
-extern int CountIntBitsF();
+int CountIntBitsF();
 
 unsigned Rotate(unsigned object, int count)
 {
     int int_bits = CountIntBitsF();
     if (count >= 0) {
         /* right rotate */
-        return (object >> count)|(object << (int_bits - count));
+        return object >> count;
     } else {
         /* left rotate */
-        return (object << count)|(object >> (int_bits - count));
+        return object << count;
     }
 }
