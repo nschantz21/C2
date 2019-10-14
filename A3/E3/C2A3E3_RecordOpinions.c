68ed335d7d2cff14dcd178d3d9d6e36b8eff5494
Nick <nschantz21@gmail.com>
2019-10-14 21:18:09 +0000

    added header comment fixed spacing and useless comment

--- A3/E3/C2A3E3_RecordOpinions.c
+++ A3/E3/C2A3E3_RecordOpinions.c
@@ -1,6 +1,17 @@
 /*
+ * Nicholas Schantz U08416544
+ * nschantz21@gmail.com
+ * C/C++ Programming II
+ * 142605 Ray Mitchell
+ * 2019-10-15
+ * C2A3E3_RecordOpinions.c
+ * Linux Ubuntu 16.04.4 LTS
+ * g++ 5.4.0
  * 
+ * Program prompts user for their ratings of a product, counts the quantity of
+ * each rating, and displays a table of the total quantity of each rating
  */
+
 #include <stdio.h>
 
 #define ENDPOINT 5
@@ -29,9 +40,8 @@ void RecordOpinions(void)
         }
     }
     /* Display table */
-   printf("Rating  Responses\n------  ---------\n");
+    printf("Rating  Responses\n------  ---------\n");
     for (int resp = 0; resp < num_of_resp; resp++) {
-        /* fix padding */
         printf("%6d  %9d\n", resp, responses[resp]);
     }
 }
