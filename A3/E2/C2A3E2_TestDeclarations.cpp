63a8e95ffcfc15699d975edf251e685be4c6b5b8
Nick <nschantz21@gmail.com>
2019-10-14 21:14:27 +0000

    added header comment

--- A3/E2/C2A3E2_TestDeclarations.cpp
+++ A3/E2/C2A3E2_TestDeclarations.cpp
@@ -1,5 +1,15 @@
 /*
+ * Nicholas Schantz U08416544
+ * nschantz21@gmail.com
+ * C/C++ Programming II
+ * 142605 Ray Mitchell
+ * 2019-10-15
+ * C2A3E2_TestDeclarations.cpp
+ * Linux Ubuntu 16.04.4 LTS
+ * g++ 5.4.0
  * 
+ * Program implements instructor defined declarations to demonstrate Reverse
+ * Right-Left rule
  */
 
 void TestDeclarations()
