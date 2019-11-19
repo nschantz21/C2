902090baf4f3abc3d64c2d88ea849b9de19de358
nick <nschantz21@gmail.com>
2019-11-19 00:33:23 +0000

    initial working version

---
+++ A8/E1/C2A8E1_MergeAndDisplay.cpp
@@ -0,0 +1,51 @@
+/*
+ * Nicholas Schantz U08416544
+ * nschantz21@gmail.com
+ * C/C++ Programming II
+ * 142605 Ray Mitchell
+ * 2019-11-19
+ * C2A8E1_MergeAndDisplay.cpp
+ * Linux Ubuntu 16.04.4 LTS
+ * g++ 5.4.0
+ * 
+ * Proceeding in order from the first file specified in files, the first line in 
+ * each file is read and When the end of any file is reached that file is closed 
+ * and the process continues using only the remaining open files until all files 
+ * have finally been closed. Empty lines are displayed as empty lines. Empty files
+ * are simply closed and ignored.
+ */
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <string.h>
+
+using namespace std;
+
+const int MAXBUFF = 512;
+const int SAFEBUF = MAXBUFF - 1;
+
+void MergeAndDisplay(ifstream files[], size_t count) {
+    /* iterate over files */
+    /* stop when they are all closed */
+    int open_count = count;
+    char * buffer = new char[MAXBUFF];
+    while (open_count > 0) {
+        for (size_t cf = 0; cf < count; cf++) {
+            if (files[cf].is_open()) {
+
+                files[cf].getline(buffer, SAFEBUF);
+                /* check if file is empty */
+                if (strlen(buffer) == 0) {
+                    /* if empty skip and close */
+                    files[cf].close();
+                    open_count--;
+                }
+                if ((strlen(buffer) != 0) && (buffer != NULL)) {
+                    cout << buffer << '\n';
+                }
+            }
+        }
+    }
+    delete buffer;
+}
---
+++ A8/E1/C2A8E1_OpenFiles.cpp
@@ -0,0 +1,44 @@
+/*
+ * Nicholas Schantz U08416544
+ * nschantz21@gmail.com
+ * C/C++ Programming II
+ * 142605 Ray Mitchell
+ * 2019-11-19
+ * C2A8E1_OpenFiles.cpp
+ * Linux Ubuntu 16.04.4 LTS
+ * g++ 5.4.0
+ * 
+ * Dynamically creates an array of ifstream objects having count elements then
+ * uses those objects to open the files named in fileNames, in order.
+ */
+
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void OpenFile(const char *fileName, ifstream &inFile)
+{
+    inFile.open(fileName);
+    /* check if file successfully opened */
+    if (!inFile.is_open()) {
+        cerr << fileName << " failed to open!\n";
+        exit(EXIT_FAILURE);
+    } else {
+        return;
+    }
+}
+
+ifstream *OpenFiles(char * const fileNames[], size_t count)
+{
+    /* create array of ifstreams */
+    static ifstream *filestreams = new ifstream[count];
+    for (size_t curr_file = 0; curr_file < count; curr_file++) {
+        /* open all  */
+        OpenFile(fileNames[curr_file], filestreams[curr_file]);
+    }
+    
+    /* return pointer to first element of filestreams */
+    return filestreams;
+}
